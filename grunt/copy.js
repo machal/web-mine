ff1414cabd1d7ffaccf85fce6d7482db891c2fcb
Martin Michálek <michalek@shortcat.cz>
2017-07-08 11:41:55 +0200

    Added Blanka and Normalize

--- grunt/copy.js
+++ grunt/copy.js
@@ -4,7 +4,17 @@ module.exports = {
 
   normalize: {
     src: 'node_modules/normalize.css/normalize.css',
-    dest: 'source/css/scss/vendor/normalize.scss',
+    dest: 'source/scss/vendor/normalize.scss'
   },
+
+  blanka: {
+    src: 'node_modules/blanka-css/blanka.css',
+    dest: 'source/scss/vendor/blanka.css'
+  },  
+
+  blanka_helpers: {
+    src: 'node_modules/blanka-css/blanka-helpers.css',
+    dest: 'source/scss/vendor/blanka-helpers.css'
+  }   
 
 };
